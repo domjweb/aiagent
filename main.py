9c59e9ddbbccef3d9d333ece03afc0a15a01f18e
domjweb <dominiquejadonwebb@gmail.com>
2025-06-17 04:57:29 -0400

    Added main functionality for system prompts and function declaration for the agent

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ parser.add_argument('--verbose',
 parser.add_argument("user_prompt")
 args = parser.parse_args()
 
-
-
 messages = [
     types.Content(role="user", parts=[types.Part(text=args.user_prompt)]),
 ]
@@ -21,16 +19,53 @@ messages = [
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
-
 client = genai.Client(api_key=api_key)
 
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
 response = client.models.generate_content(
     model='gemini-2.0-flash-001',
-    contents=messages
+    contents=messages,
+    config=types.GenerateContentConfig(
+    tools=[available_functions], system_instruction=system_prompt
+)
 )
 
-print(response.text)
+if response.function_calls:
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+else:
+    print(response.text)
+
 
 if args.verbose == True:
     print(f"User prompt: {args.user_prompt}")
